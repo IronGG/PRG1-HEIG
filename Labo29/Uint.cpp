1c73ae770c151a6d94b7e9e7fff2c569e97c83ad
IronG <adclaren1@gmail.com>
2022-01-10 06:27:00 +0100

    Labo29 init

---
+++ Labo29/Uint.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adri405 on 1/10/2022.
+//
+
+#include "Uint.h"
---
+++ Labo29/Uint.h
@@ -0,0 +1,14 @@
+//
+// Created by adri405 on 1/10/2022.
+//
+
+#ifndef LABO29_UINT_H
+#define LABO29_UINT_H
+
+
+class Uint {
+
+};
+
+
+#endif //LABO29_UINT_H
