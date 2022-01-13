d2465a8c0f077c56d849dc362bd0b4948833c6a4
GrosPatapouf <victor_g18@hotmail.fr>
2022-01-13 20:37:53 +0100

    mon code

--- Labo29/Uint.cpp
+++ Labo29/Uint.cpp
@@ -1,56 +1,39 @@
 //
-// Created by adri405 on 1/10/2022.
+// Created by victo on 12.01.2022.
 //
 
 #include "Uint.h"
 
-//int soustraction(Uint a, Uint b)
-//{
-//    // Si a est
-//
-//    if (Uint a -)
-//}
-//
-//
-//bool comparaison(Uint a, Uint b)
-//{
+Uint::Uint() {
+
+}
+
+Uint Uint::operator+(const Uint &addition) const {
+
+    uint64_t retenue = 0;
+    Uint temp = 0;
+
+    for (uint64_t i = 0; i < valeur.size(); ++i) {
+        if (valeur.at(i) + addition.valeur.at(i) + retenue >= 10) {
+            temp.valeur.at(i) = (valeur.at(i) + addition.valeur.at(i)) % 10 + retenue;
+            retenue = (valeur.at(i) + addition.valeur.at(i)) / 10;
+        } else {
+            temp.valeur.at(i) = valeur.at(i) + addition.valeur.at(i) + retenue;
+            retenue = 0;
+        }
+    }
+    return temp;
+}
+
+//Uint Uint::comp(Uint a, Uint b) {
 //    if(a < b)
 //    {
 //        return -1;
-//    }
-//    else if(a == b)
-//    {
+//    }else if(a > b){
+//        return 1;
+//    }else if (a == b){
 //        return 0;
 //    }
-//    else
-//    {
-//        return 1;
-//    }
-//
 //}
-Uint Uint::operator+(const Uint &val) const {
 
-    int retenue = 0;
-    Uint temp = 0;
-
-    for (int i = 0; i < 499; ++i) {
-
-        if (this->valeur[i] + val.valeur[i] <= 10) {
-            temp.valeur[i] = (this->valeur[i] + val.valeur[i]) % 10 + retenue;
-            retenue = (this->valeur[i] + val.valeur[i]) / 10;
-        } else {
-            temp.valeur[i] = this->valeur[i] + val.valeur[i] + retenue;
-            retenue = 0;
-        }
-
-    }
-
-        return temp;
-
-    }
-
-    //Uint temp;
-    //temp.valeur = this->valeur + val.valeur;
-
-    //return temp;
 
--- Labo29/Uint.h
+++ Labo29/Uint.h
@@ -1,114 +1,86 @@
-//
-// Created by adri405 on 1/10/2022.
-//
 
-#ifndef LABO29_UINT_H
-#define LABO29_UINT_H
+#ifndef NICOLET_LABO29_CPP_UINT_H
+#define NICOLET_LABO29_CPP_UINT_H
 
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using std::string;
 using std::vector;
 
 class Uint {
 
-public:
-    // Constructeurs
+public :
+    //Constructeurs
     Uint();
-    Uint(std::string stringVal) {
 
-        // TODO: Inverser l'ordre !!
-
-/*        std::cout << stringVal[0];
-        std::cout << stringVal[1];
-        std::cout << stringVal[2];
-        std::cout << stringVal.length();*/
-
-        for (int i = 0; i <= stringVal.length() - 1; ++i) {
-
-            //std::cout << std::endl;
-
-            this->valeur[i] = stringVal[i] - '0';
-
-/*            std::cout << stringVal[0];
-            std::cout << stringVal[1];
-            std::cout << stringVal[2];
-            std::cout << "---";
-            std::cout << this->valeur[0];
-            std::cout << this->valeur[1];
-            std::cout << this->valeur[2];*/
+    Uint(uint64_t varUint64) {
+        valeur.clear();
+        for (uint64_t i = 1; i <= varUint64; i *= 10) {
+            this->valeur.push_back(varUint64 / i % 10);
         }
-
-    } // TODO: trouver une solution pour le string
-
-
-    Uint(uint64_t uintVal) {
-
-        int j = 0;
-        for(long long i = 1; i <= uintVal; i *= 10)
-        {
-            this->valeur[j] = (uintVal / i) % 10; // 34 % 10 = 4 ^||| 34 / 10 = 4587 -> divise par i
-            j++;
-        }
+        std::reverse(valeur.begin(), valeur.end());
+        valeur.shrink_to_fit();
     }
 
-    //  constructeur de copie
+    Uint(std::string varString) {
+        varString.shrink_to_fit();
+        valeur.clear();
+        for (char i: varString) {
+            this->valeur.push_back(i - '0');
+        }
+        std::reverse(valeur.begin(), valeur.end());
+        valeur.shrink_to_fit();
+    }
 
-    // destructeur
+    //Operateurs
 
+    Uint operator+(const Uint &addition) const;
 
-    //opérateurs
-    Uint operator+(const Uint& valeur) const;
-    Uint operator-(const Uint& valeur) const;
-    Uint operator=(const Uint& valeur) const;
-    Uint operator*(const Uint& valeur) const;
+    Uint operator-(const Uint &soustraction) const;
 
-    Uint operator<(const Uint& valeur) const;
-    Uint operator<=(const Uint& valeur) const;
-    Uint operator>(const Uint& valeur) const;
-    Uint operator>=(const Uint& valeur) const;
-    Uint operator==(const Uint& valeur) const;
+    Uint operator=(const Uint &egalite) const;
 
+    Uint operator*(const Uint &multiplication) const;
+
+    Uint operator<(const Uint &plusPetitQue) const;
+
+    Uint operator<=(const Uint &plusPetitQueEgale) const;
+
+    Uint operator>(const Uint &plusGrandQue) const;
+
+    Uint operator>=(const Uint &plusGrandQueEgale) const;
+
+    Uint operator==(const Uint &valeur) const;
+
+    //fonctions
+    //Uint comp(Uint a, Uint b);
     void affichage() {
-
-
-/*        int x = 0;
-        while(valeur[x] != 0)
-        {
-            std::cout << valeur[x];
-            x++;
-        }*/
-
         std::cout << std::endl << "--------------" << std::endl;
 
         std::cout << "le nombre est : ";
 
-        bool display = false;
-        for (int i = 499; i >= 0; i--) {
-            if(display){
-                std::cout << valeur[i];
-            }
-            else if(valeur[i] != 0)
-            {
-                std::cout << valeur[i];
-                display = true;
-            }
-        }
+        for (int i = 0; i < valeur.size(); i++)
+            std::cout << valeur.at(i);
 
         std::cout << std::endl;
 
         std::cout << "--------------" << std::endl;
-        //std::cout << valeur[0];
-        //std::cout << valeur[1];
-
     };
 
-private:
-    int valeur[500] = {0}; // taille a changer ou trouver solution
+    void capacity() {
+        std::cout << "--------------" << std::endl;
+        std::cout << "la capacity est : ";
+        std::cout << valeur.capacity();
+        std::cout << std::endl;
+        std::cout << "--------------" << std::endl;
+    };
 
+private :
+    std::vector<int> valeur;
 };
 
 
-#endif //LABO29_UINT_H
+#endif //NICOLET_LABO29_CPP_UINT_H
